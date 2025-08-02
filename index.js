818357bc57027236ad0e9b335dd8880207753307
Aryan Singh <137031264+gambare2@users.noreply.github.com>
2025-08-02 16:47:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('./db/index');
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "https://music-tube.vercel.app/",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
