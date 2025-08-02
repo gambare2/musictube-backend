8aa1af3fccfb1b77842e178dfc272e9f1765cdbd
Aryan Singh <137031264+gambare2@users.noreply.github.com>
2025-08-02 15:56:43 +0530

    new commit

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 const express = require('express');
 require('dotenv').config();
 require('./db/index');
-const bodyparser = require('body-parser');
 const cors = require('cors');
 const AuthRouter = require('./Router/AuthRouter');
 
+const app = express();
 
-const app= express();
-
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send('Backend for pritube music');
+app.get('/', (req, res) => {
+  res.send('Backend for pritube music');
 });
 
-app.use(cors(
-    {
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    }
-));
-app.use(bodyparser.json());
+app.use('/auth', AuthRouter);
 
-app.use('/auth', AuthRouter)
-
-
-PORT = process.env.PORT || 5001;
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
