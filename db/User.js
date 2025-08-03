aa797e159d5e4393cc63db4d9278e5f5c1336882
Aryan Singh <137031264+gambare2@users.noreply.github.com>
2025-08-03 12:53:18 +0530

    audio and tracks

--- db/User.js
+++ db/User.js
@@ -22,7 +22,12 @@ const userSchema = new Schema({
     },
     DOB: {
         type: Date,
-    }
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
 })
 
 const UserModal = mongoose.model('User', userSchema);
